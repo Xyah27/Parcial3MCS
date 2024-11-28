59c3f2f76a3d1882183b4cd90a396ef64c990438
Xyah27 <99484123+Xyah27@users.noreply.github.com>
2024-11-27 20:43:29 -0500

    Add files via upload

---
+++ Parte final/microservice-one/src/test/java/com/co/microservice_one/MicroserviceOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.microservice_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
