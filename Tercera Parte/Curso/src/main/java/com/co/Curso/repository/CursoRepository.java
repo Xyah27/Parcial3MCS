aaaceeee50f6fcb25a03ce6bdd75429e407cae9f
Xyah27 <99484123+Xyah27@users.noreply.github.com>
2024-12-03 11:32:24 -0500

    Delete Tercera Parte directory

--- Tercera Parte/Curso/src/main/java/com/co/Curso/repository/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.co.Curso.repository;
-
-import com.co.Curso.models.entity.Curso;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CursoRepository extends CrudRepository<Curso, Long> {
-}
