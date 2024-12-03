d3df310f5e2d733e0d7694dcaac4a9c8ce646656
Xyah27 <99484123+Xyah27@users.noreply.github.com>
2024-12-03 11:32:08 -0500

    Delete Primera Parte directory

--- Primera Parte/usuario/src/main/java/com/co/usuario/repository/AlumnoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.co.usuario.repository;
-import com.co.commonservice.models.entity.Alumno;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
-}
