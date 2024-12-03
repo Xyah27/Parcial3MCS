76c376c1d9ff3a0e0bfcf34a0ca5d0224e3d5cb0
Xyah27 <99484123+Xyah27@users.noreply.github.com>
2024-12-03 13:01:14 -0500

    Delete MicroServices_FinalProject directory

--- MicroServices_FinalProject/curso/src/main/java/rico/example/curso/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package rico.example.curso.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import models.entity.Curso;
-
-public interface CursoRepository  extends CrudRepository<Curso, Long> {
-}
