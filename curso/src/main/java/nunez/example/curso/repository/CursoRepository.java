6ac4e23b0f1124d67c204a36557d011da2f06a7b
Xyah27 <99484123+Xyah27@users.noreply.github.com>
2024-12-03 20:51:30 -0500

    Delete curso directory

--- curso/src/main/java/nunez/example/curso/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package nunez.example.curso.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import models.entity.Curso;
-
-public interface CursoRepository  extends CrudRepository<Curso, Long> {
-}
