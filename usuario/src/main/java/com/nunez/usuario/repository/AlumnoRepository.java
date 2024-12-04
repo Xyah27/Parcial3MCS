8d01b63ba077257606a48f3ee3a00871fc2b95bd
Xyah27 <99484123+Xyah27@users.noreply.github.com>
2024-12-03 20:51:56 -0500

    Delete usuario directory

--- usuario/src/main/java/com/nunez/usuario/repository/AlumnoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nunez.usuario.repository;
-
-import models.entity.Alumno;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
-}
